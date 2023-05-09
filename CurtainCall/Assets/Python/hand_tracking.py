87ed9a5e0209f5c0acc3bfad699497580494526e
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-05-09 16:51:07 +0900

    feat : mediapipe를 이용한 hand tracking

--- CurtainCall/Assets/Python/hand_tracking.py
+++ CurtainCall/Assets/Python/hand_tracking.py
@@ -1,4 +1,10 @@
 import cv2 as cv
+import numpy as np
+import mediapipe as mp
+
+# prepare hand tracking model
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
 
 # 카메라 장치 연결 및 출력 설정
 capture = cv.VideoCapture(0)
@@ -14,6 +20,16 @@ while cv.waitKey(33) < 0:
     if not ret:
         break
     
+    # applying hand tracking model => openCV는 BGR, mediapipe는 RGB 체제를 사용하므로 Convert 필요
+    edited_frame = cv.cvtColor(edited_frame, cv.COLOR_BGR2RGB)
+    hand_tracking_results = mp_hands.Hands().process(edited_frame)
+    edited_frame = cv.cvtColor(edited_frame, cv.COLOR_RGB2BGR)
+
+    # drawing landmarks on the frame
+    if hand_tracking_results.multi_hand_landmarks:
+        for hand_landmarks in hand_tracking_results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(edited_frame, hand_landmarks, connections=mp_hands.HAND_CONNECTIONS)
+
     # 좌우 반전하여 거울처럼 출력 함
     edited_frame = cv.flip(edited_frame, 1)
     cv.imshow("Camera", edited_frame)
